238d1963df6488edcad510e3246a9fd5a0295809
Mythili <mnishtala@castlighthealth.com>
2023-04-24 13:53:18 +0530

    Print request headers

--- src/main/java/com/vesterales/authserver/controller/EventNotificationController.java
+++ src/main/java/com/vesterales/authserver/controller/EventNotificationController.java
@@ -5,14 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @Slf4j
 public class EventNotificationController {
 
     @PostMapping("/sfmc-events")
-    public ResponseEntity<String> response(@RequestBody Object eventBody) {
+    public ResponseEntity<String> response(@RequestHeader Map<String,String> headers, @RequestBody Object eventBody) {
+        log.info("Request headers {}", headers);
         log.info("Event received from ENS :{}", eventBody);
         return ResponseEntity.status(HttpStatus.OK).body("Hello There");
     }
